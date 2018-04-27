ecf192fdaca6b0bc3f3795a2e4e30b1bfcc153da
Zax37 <c37zax@gmail.com>
2018-04-27 08:51:49 +0200

    Changed way of supplying TransactionWriter.

--- src/main/java/uj/jwzp/w2/e3/logic/beans/TransactionWriterSupplier.java
+++ src/main/java/uj/jwzp/w2/e3/logic/beans/TransactionWriterSupplier.java
@@ -7,21 +7,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import uj.jwzp.w2.e3.logic.writer.JSONTransactionWriter;
 import uj.jwzp.w2.e3.logic.writer.TransactionWriter;
+import uj.jwzp.w2.e3.logic.writer.XMLTransactionWriter;
+import uj.jwzp.w2.e3.logic.writer.YAMLTransactionWriter;
 
 @Lazy
 @Service
 public class TransactionWriterSupplier {
-    private BeanFactory beans;
+    private JSONTransactionWriter jsonWriter;
+    private XMLTransactionWriter xmlWriter;
+    private YAMLTransactionWriter yamlWriter;
 
     @Autowired
-    TransactionWriterSupplier(BeanFactory beans) {
-        this.beans = beans;
+    TransactionWriterSupplier(
+            JSONTransactionWriter jsonWriter,
+            XMLTransactionWriter xmlWriter,
+            YAMLTransactionWriter yamlWriter
+    ) {
+        this.jsonWriter = jsonWriter;
+        this.xmlWriter = xmlWriter;
+        this.yamlWriter = yamlWriter;
     }
 
     @Bean
     @Primary
-    public TransactionWriter getCorrectTransactionWriter(@Value("${format}") String format) {
-        return beans.getBean(format, TransactionWriter.class);
+    public TransactionWriter getCorrectTransactionWriter(
+            @Value("${format}") TransactionWriter.AllowedOutputFormats format
+    ) {
+        switch (format) {
+            case json:
+            default:
+                return jsonWriter;
+            case xml:
+                return xmlWriter;
+            case yaml:
+                return yamlWriter;
+        }
     }
 }
